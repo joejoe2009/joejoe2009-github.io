2f293bfad7831445e0c2a813ba585fa4c2f6ac65
Joseph Itiose Account <iti18001@byui.edu>
2019-03-20 05:51:45 +0100

    update

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,5 +1,5 @@
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.open =('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5061036&appid=02acf3eba1e20146f0800379e8cb831c&units=imperial',true);
+weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5061036&appid=02acf3eba1e20146f0800379e8cb831c&units=imperial',true);
 
 weatherRequest.send();
 
--- lesson-10/js/weathercurrent.js
+++ lesson-10/js/weathercurrent.js
@@ -1,5 +1,5 @@
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.open =('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5061036&appid=02acf3eba1e20146f0800379e8cb831c&units=imperial',true);
+weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5061036&appid=02acf3eba1e20146f0800379e8cb831c&units=imperial',true);
 
 weatherRequest.send();
 
@@ -7,8 +7,11 @@ weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('cloudy').innerHTML = weatherData.main.temp;
-    
+    document.getElementById('currently').innerHTML = weatherData.weather[0].main;
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+    document.getElementById('current-humi').innerHTML = weatherData.main.humidity;
+    document.getElementById('current-wind').innerHTML = weatherData.wind.speed;
+
 
 }
 
