be5f2b64f84672d151b1c35eb21a121182439276
Joseph Itiose Account <iti18001@byui.edu>
2019-04-13 03:44:40 +0100

    update

--- final-project/js/Auweather.js
+++ final-project/js/Auweather.js
@@ -14,19 +14,3 @@ weatherRequest2.onload =  function () {
 
 
 }
-
-
-// windchill
-
-function doInputOutput() {
-    var tempF = parseFloat(document.getElementById('tempFInputBox2').value);
-    var speed =  parseFloat(document.getElementById('speedInputBox2').value);
-    var result = windChill(tempF, speed);
-    document.getElementById('outputDiv2').innerHTML = result;
-}
-
-function windChill(tempF, speed) { 
-return   35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 
-
-0.16);
-}
--- final-project/js/Ghweather.js
+++ final-project/js/Ghweather.js
@@ -14,19 +14,3 @@ weatherRequest1.onload =  function () {
 
 
 }
-
-
-// windchill
-
-function doInputOutput() {
-    var tempF = parseFloat(document.getElementById('tempFInputBox1').value);
-    var speed =  parseFloat(document.getElementById('speedInputBox1').value);
-    var result = windChill(tempF, speed);
-    document.getElementById('outputDiv1').innerHTML = result;
-}
-
-function windChill(tempF, speed) { 
-return   35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 
-
-0.16);
-}
